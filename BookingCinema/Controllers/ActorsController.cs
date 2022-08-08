317feef9274c63b920390d2ad4945e75f3b363ca
Mohamed Atef <mohamedatef223366@gmail.com>
2022-08-09 00:07:56 +0200

    Revert "Add Actors View"
    This reverts commit caffbf0303ff99f5ec63fa03b4e14d5909b3b79c.

--- BookingCinema/Controllers/ActorsController.cs
+++ BookingCinema/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace BookingCinema.Controllers
 
         public IActionResult Index()
         {
-            var data = _context.Actors;
+            var data = _context.Actors.ToList();
             return View(data);
         }
     }
