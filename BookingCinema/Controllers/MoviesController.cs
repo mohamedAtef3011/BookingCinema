2ac977ae42dc1df21f1f01410c653a0ecfdd9ec8
Mohamed Atef <mohamedatef223366@gmail.com>
2022-08-08 19:39:39 +0200

    Adding Actors,Cinema,Producers and Movies Controllers

---
+++ BookingCinema/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using BookingCinema.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingCinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+
+            return View(movies);
+        }
+    }
+}
