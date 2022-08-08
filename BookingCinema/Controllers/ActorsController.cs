caffbf0303ff99f5ec63fa03b4e14d5909b3b79c
Mohamed Atef <mohamedatef223366@gmail.com>
2022-08-09 00:06:40 +0200

    Add Actors View

--- BookingCinema/Controllers/ActorsController.cs
+++ BookingCinema/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace BookingCinema.Controllers
 
         public IActionResult Index()
         {
-            var data = _context.Actors.ToList();
+            var data = _context.Actors;
             return View(data);
         }
     }
